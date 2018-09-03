1b815dd6d73bfd48985bd98bb86fee50fdca0033
Tim Swart <TimSwart@users.noreply.github.com>
2018-09-02 18:25:19 -0700

    update test var names

--- tests/test_arm_file.py
+++ tests/test_arm_file.py
@@ -25,54 +25,66 @@ def test_init_check_src_link_local_file():
     ArmFile('tests/test_template.json', 'test_template')
 
 def test_get_value_valid():
-    j = ArmFile('tests/test_template.json')
-    assert j.get_value('parameters.storageAccountType.defaultValue') == 'Standard_LRS'
+    arm = ArmFile('tests/test_template.json')
+    assert arm.get_value('parameters.storageAccountType.defaultValue') == 'Standard_LRS'
 
 def test_get_value_changed_outside_of_class():
-    j = ArmFile('tests/test_template.json')
-    v = j.get_value('parameters.storageAccountType')
+    arm = ArmFile('tests/test_template.json')
+    v = arm.get_value('parameters.storageAccountType')
     v['type'] = 'changed'
-    assert v != j.get_value('parameters.storageAccountType')
+    assert v != arm.get_value('parameters.storageAccountType')
 
 def test_get_value_error():
-    j = ArmFile('tests/test_template.json')
+    arm = ArmFile('tests/test_template.json')
     with pytest.raises(KeyError):
-        j.get_value('parameters.storageAccountType.badValue')
+        arm.get_value('parameters.storageAccountType.badValue')
 
 def test_get_value_error_2():
-    j = ArmFile('tests/test_template.json')
+    arm = ArmFile('tests/test_template.json')
     with pytest.raises(KeyError):
-        j.get_value('parameters.storageAccountType.defaultValue.bad')
+        arm.get_value('parameters.storageAccountType.defaultValue.bad')
 
 def test_get_value_no_key():
-    j = ArmFile('tests/test_template.json')
-    assert j.get_value('') == j._ArmFile__data
+    arm = ArmFile('tests/test_template.json')
+    assert arm.get_value('') == arm._ArmFile__data
 
 def test_set_value():
-    j = ArmFile('tests/test_template.json')
-    j.set_value('parameters.storageAccountType.defaultValue', 'changed')
-    assert 'changed' == j.get_value('parameters.storageAccountType.defaultValue')
+    arm = ArmFile('tests/test_template.json')
+    arm.set_value('parameters.storageAccountType.defaultValue', 'changed')
+    assert 'changed' == arm.get_value('parameters.storageAccountType.defaultValue')
 
 def test_set_value_bad_key():
-    j = ArmFile('tests/test_template.json')
+    arm = ArmFile('tests/test_template.json')
     with pytest.raises(KeyError):
-        j.set_value('parameters.storageAccountType.defaultValue.bad', 'changed')
+        arm.set_value('parameters.storageAccountType.defaultValue.bad', 'changed')
+
+def test_set_value_list():
+    arm = ArmFile('tests/test_template.json')
+    key = 'resources'
+    my_list = ['a', 'b', 'c']
+    arm.set_value(key, my_list)
+    changes = arm.get_change_log()
+    old = 'old_value'
+    new = 'new_value'
+    assert changes[key][old] != changes[key][new]
+    assert changes[key][new] == my_list
+    assert arm.get_value(key) == my_list
 
 def test_set_value_log_change():
-    j = ArmFile('tests/test_template.json')
+    arm = ArmFile('tests/test_template.json')
     key = 'parameters.storageAccountType.defaultValue'
-    j.set_value(key, 'changed')
-    assert key in j._ArmFile__changed_values
+    arm.set_value(key, 'changed')
+    assert key in arm._ArmFile__changed_values
 
-def test():
-    j = ArmFile('tests/test_template.json')
+def test_change_log():
+    arm = ArmFile('tests/test_template.json')
     key = 'parameters.storageAccountType.defaultValue'
     key2 = 'parameters.location.type'
     old = 'old_value'
     new = 'new_value'
-    j.set_value(key, 'changed')
-    j.set_value(key2, 'updated')
-    changes = j.get_change_log()
+    arm.set_value(key, 'changed')
+    arm.set_value(key2, 'updated')
+    changes = arm.get_change_log()
     assert changes[key][old] == 'Standard_LRS'
     assert changes[key][new] == 'changed'
     assert changes[key2][old] == 'string'
